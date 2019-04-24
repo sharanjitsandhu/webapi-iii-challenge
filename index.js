6e2d93deea0dce084af2a8b2ee1f7e37bf1bffc4
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-04-24 12:37:06 -0700

    Server is up and running

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code away!
+const server = require("./server.js");
+
+// server listening
+server.listen(5000, () => {
+  console.log("\n*** Server is running on http://localhost:5000 ***\n");
+});
